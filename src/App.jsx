0865ca3f80166879ae326bc46be8e329f979f068
Vorapon-Pon <vorapon.w.pon@gmail.com>
2025-02-16 22:10:50 +0700

    Navbar1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Auth from './components/Auth';
 import Account from './components/Account';
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <div>
       Hello World!
       </div>
---
+++ src/components/navbar.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-neutral-200 opacity-80 shadow-md fixed top-0 left-0 w-full transition-all duration-300 z-10 h-18">
+      <div className="bg-white shadow-md max-w-6xl mx-auto px-6 flex justify-between items-center h-14 my-2 rounded-full">
+        {/* LOGO */}
+        <h1 className="text-xl text-[#FF7E69] font-bold">North's Club</h1>
+
+        {/* Desktop Menu */}
+        <div className="hidden md:flex space-x-6 text-[#FF7E69]">
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">หน้าหลัก</a>
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">ชมรม</a>
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">เอกสาร</a>
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">สถิติ</a>
+          <button className="bg-[#7CE9BF] text-black font-bold px-4 py-1 rounded-full hover:bg-emerald-400">
+            Login
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden flex flex-col bg-white shadow-md py-4 space-y-4 text-center">
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">หน้าหลัก</a>
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">ชมรม</a>
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">เอกสาร</a>
+          <a href="#" className="hover:underline hover:text-[#7CE9BF]">สถิติ</a>
+          <button className="bg-[#7CE9BF] text-black font-bold px-4 py-1 rounded-full hover:bg-emerald-400 mx-auto">
+            Login
+          </button>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
