a78983f63dbcbc6e141576af55896fcd9b370227
Phumipat Jitreephot <Phumipat30439@gmail.com>
2025-02-22 16:11:00 +0700

    add role base access on wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/clubs" element={<Clublist />} />
-          <Route path="/createClub" element={<Wrapper><CreateClub /></Wrapper>} />
+          <Route path="/createClub" element={<Wrapper allowedRoles={['student', 'club', 'admin']} ><CreateClub /></Wrapper>} />
         </Routes>
         <Footer />
       </Router>
