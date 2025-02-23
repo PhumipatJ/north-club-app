090c25c6b6d85710b6a4a818b0ea000d339a20a5
Phumipat Jitreephot <Phumipat30439@gmail.com>
2025-02-23 12:07:42 +0700

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Clubfile from './components/sections/Clubfile';
 import Clubpage from './components/sections/Clubpage';
 import Wrapper from './components/sections/Wrapper';
 import Footer from './components/Footer';
+import CreateClub from './components/sections/createClub';
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route path="/clubs" element={<Clublist />} />
           <Route path="/clubs/:clubname" element={<Clubpage />} />
           <Route path="/docs" element={<Clubfile />} />
+          <Route path="/createClub" element={<CreateClub />} />
         </Routes>
         <Footer />
       </Router>
