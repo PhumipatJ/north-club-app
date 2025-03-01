5ddff8e29b0bb4616c53eb0ab1a78aa49b9e3070
Phumipat Jitreephot <Phumipat30439@gmail.com>
2025-03-01 08:48:35 +0700

    delete approvalDetail route

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ import ClubApplication from './components/sections/ClubApplication';
 import AdminApprove from './components/sections/AdminApprove';
 import AdminRespond from './components/sections/AdminRespond';
 import ApprovalHistory from './components/sections/ApprovalHistory';
-import ApprovalDetail from './components/sections/ApprovalDetail';
 import Clubprofile from './components/sections/Clubprofile';
 import ActivityDetail from './components/sections/ActivityDetail';
 function App() {
@@ -41,7 +40,6 @@ function App() {
           <Route path="/database" element={<Wrapper allowedRoles={['admin']} ><AdminApprove /></Wrapper>} />
           <Route path="/database/adminRespond" element={<Wrapper allowedRoles={['admin']} ><AdminRespond /></Wrapper>} />
           <Route path="/database/approvalHistory" element={<Wrapper allowedRoles={['admin']} ><ApprovalHistory /></Wrapper>} />
-          <Route path="/database/approvalDetail/:clubId" element={<Wrapper allowedRoles={['admin']} ><ApprovalDetail /><Footer /></Wrapper>} />
           <Route path="/userprofile" element={<Wrapper allowedRoles={['student','club','admin']}><UserProfile /><Footer /></Wrapper>} />
           <Route path="/Clubprofile" element={<Wrapper allowedRoles={['student','club','admin']}><Clubprofile /><Footer /></Wrapper>} />
           <Route path="/activityDetail" element={<><ActivityDetail /><Footer /></>} />
