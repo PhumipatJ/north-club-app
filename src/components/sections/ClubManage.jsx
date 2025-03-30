e41bd2ac8445a78f804f3045dadace4dcdf0a6f0
Phumipat Jitreephot <Phumipat30439@gmail.com>
2025-03-30 20:05:28 +0700

    try catch on data split

--- src/components/sections/ClubManage.jsx
+++ src/components/sections/ClubManage.jsx
@@ -79,7 +79,8 @@ const ClubManage = ({userinfo}) => {
             upcomingEvents.push(event);
           }
         });
-  
+        
+        try {
         // Sort non-expired events by start_date and then start_time
         upcomingEvents.sort((a, b) => {
           const [aDay, aMonth, aYear] = a.start_date.split("/").map(Number);
@@ -92,7 +93,8 @@ const ClubManage = ({userinfo}) => {
           }
           return 0;
         });
-  
+        
+        
         // Sort expired events by end_date and then end_time
         expiredEvents.sort((a, b) => {
           const [aDay, aMonth, aYear] = a.end_date.split("/").map(Number);
@@ -102,6 +104,9 @@ const ClubManage = ({userinfo}) => {
   
           return aEnd - bEnd; // Sort by end_date and end_time
         });
+        } catch (error) {
+          //console.error("Error sorting expiredEvents:", error);
+        }
   
         //console.log(upcomingEvents);
         //console.log(expiredEvents);
